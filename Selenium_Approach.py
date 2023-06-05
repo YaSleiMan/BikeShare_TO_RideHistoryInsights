9f3e182d03e850625b65810e5524116119e7e22b
Yasser <yass99@gmail.com>
2023-06-04 23:48:15 -0500

    Added loop for dates

--- Selenium_Approach.py
+++ Selenium_Approach.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+from datetime import datetime, timedelta
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
@@ -25,7 +26,7 @@ username_field = driver.find_element(By.ID, 'userName')
 password_field = driver.find_element(By.ID, 'password')
 submit_button = driver.find_element(By.CSS_SELECTOR, 'input.button.sign-in.w-button[type="submit"]')
 
-username_field.send_keys('')
+username_field.send_keys('@gmail.com')
 password_field.send_keys('')
 submit_button.click()
 time.sleep(2)
@@ -33,21 +34,26 @@ time.sleep(2)
 # Find the dropdown element by ID
 dropdown = Select(driver.find_element(By.ID, 'period'))
 dropdown.select_by_value('custom')
-startTime_field = driver.find_element(By.ID, 'date[start]')
-endTime_field = driver.find_element(By.ID, 'date[end]')
 
-startTime = "01-05-2023"
-endTime = "30-05-2023"
-startTime_field.send_keys(startTime)
-endTime_field.send_keys(endTime)
-actions = ActionChains(driver)
+startTime = datetime(2023, 5, 1)
+endTime = datetime(2023, 5, 30)
+timeDelta = endTime - startTime
+iterations = timeDelta.days
+queryWindow = 7
 
-# Click somewhere
-actions.move_by_offset(0, 0).click().perform()
-time.sleep(1)
+for i in range(round(iterations/queryWindow)+1):
+    startTime_field = driver.find_element(By.ID, 'date[start]')
+    endTime_field = driver.find_element(By.ID, 'date[end]')
+    startTime_field.send_keys(startTime.strftime('%d-%m-%Y'))
+    startTime = startTime + timedelta(days=queryWindow)
+    endTime_field.send_keys(startTime.strftime('%d-%m-%Y'))
+    actions = ActionChains(driver)
 
-# Copy page contents
-page_contents = driver.page_source
-print(page_contents)
+    # Click somewhere
+    actions.move_by_offset(0, 0).click().perform()
+    time.sleep(1)
+    # # Copy page contents
+    # page_contents = driver.page_source
+    # print(page_contents)
 
 driver.quit()
