228e3cbe5df7b01fb4fb1606d9637ddd964ce49c
Yasser <yass99@gmail.com>
2023-06-10 20:30:45 -0500

    Can now export all relevant data as an txt file

--- Selenium_GetData.py
+++ Selenium_GetData.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 from datetime import datetime, timedelta
-from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
@@ -41,6 +41,10 @@ timeDelta = endTime - startTime
 iterations = timeDelta.days
 queryWindow = 7
 
+start_date_texts = []
+duration_texts = []
+station_name_start_texts = []
+station_name_end_texts = []
 for i in range(round(iterations/queryWindow)+1):
     startTime_field = driver.find_element(By.ID, 'date[start]')
     endTime_field = driver.find_element(By.ID, 'date[end]')
@@ -52,8 +56,37 @@ for i in range(round(iterations/queryWindow)+1):
     # Click somewhere
     actions.move_by_offset(0, 0).click().perform()
     time.sleep(1)
-    # # Copy page contents
-    # page_contents = driver.page_source
+    # Copy page contents
+    page_contents = BeautifulSoup(driver.page_source, 'html.parser')
     # print(page_contents)
 
+    # Get elements
+    start_dates = page_contents.find_all('div', class_='start-date')
+    start_date_texts = start_date_texts + [j.text.strip() for j in start_dates]
+    duration = page_contents.find_all('div', class_='duration')
+    duration_texts = duration_texts + [j.text.strip() for j in duration]
+    station_name_start = page_contents.find_all('div', class_='start-station-name-block')
+    station_name_start_texts = station_name_start_texts + [j.text.strip() for j in station_name_start if j.text.strip() != '' and not j.text.strip().startswith("Bike ID")]
+    station_name_end = page_contents.find_all('div', class_='end-station-name-block')
+    station_name_end_texts = station_name_end_texts + [j.text.strip() for j in station_name_end if j.text.strip() != '' and not j.text.strip().startswith("Price")]
+
 driver.quit()
+
+print(start_date_texts)
+print(duration_texts)
+print(station_name_start_texts)
+print(station_name_end_texts)
+print(len(start_date_texts))
+print(len(duration_texts))
+print(len(station_name_start_texts))
+print(len(station_name_end_texts))
+
+with open('list_file.txt', 'w') as file:
+    file.write(', '.join(start_date_texts))
+    file.write('\n')
+    file.write(', '.join(duration_texts))
+    file.write('\n')
+    file.write(', '.join(station_name_start_texts))
+    file.write('\n')
+    file.write(', '.join(station_name_end_texts))
+
